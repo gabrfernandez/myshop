b42d101148a25a38e4be916918fdeca82977d1b6
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-10-22 15:12:08 -0700

    db connect

---
+++ backend/config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        console.log(`MONGODB Connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
+module.exports = connectDB
+
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useFindAndModify: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+// })
+//     .then(() => console.log('Database connection established'))
+//     .catch(err => console.log('There was an error', err));
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
-const express = require('express') //common js, import es modules 
+const express = require('express') //common js
+const dotenv = require('dotenv')
 const products = require("./data/products")
+const connectDB = require('./config/db')
+
+// import express from 'express' es module
+// import dotenv from 'dotenv'
+// import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -16,4 +26,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(products)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
