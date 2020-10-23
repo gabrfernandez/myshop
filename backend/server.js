126683d16de7bd8c04d8d756f165631d1d5e4842
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-10-23 13:45:42 -0700

    errrors

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const colors = require('colors')
 const connectDB = require('./config/db')
 
 const productRoutes = require('./routes/productRoutes')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 dotenv.config()
 
@@ -12,11 +13,16 @@ connectDB()
 
 const app = express()
 
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
