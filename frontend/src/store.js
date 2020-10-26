ae38af42b501c03251b91b95a8719c6ffe6b4c9a
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-10-25 22:53:21 -0700

    place order page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -23,7 +23,7 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null
 
-const shippingAddressFromStorage = localStorage.getItem('shippingAddressFromStorage')
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
     ? JSON.parse(localStorage.getItem('shippingAddress'))
     : {}
 
