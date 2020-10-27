1025f201edafc669704dbb26062126e9075e0790
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-10-27 00:03:33 -0700

    update admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,6 @@ const { protect, admin } = require('../middleware/authMiddleware')
 router.route('/').post(userCtrl.registerUser).get(protect, admin, userCtrl.getUsers)
 router.route('/login').post(userCtrl.authUser)
 router.route('/profile').get(protect, userCtrl.getUserProfile).put(protect, userCtrl.updateUserProfile)
-router.route('/:id').delete(protect, admin, userCtrl.deleteUser)
+router.route('/:id').delete(protect, admin, userCtrl.deleteUser).get(protect, admin, userCtrl.getUserById).put(protect, admin, userCtrl.updateUser)
 
 module.exports = router
