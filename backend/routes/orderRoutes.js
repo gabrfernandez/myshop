2f4ed41d7feff5e1e9113ea61534815cf2e8ae21
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-10-26 17:52:49 -0700

    order screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,5 +4,7 @@ const orderCtrl = require('../controllers/orderController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').post(protect, orderCtrl.addOrderItems)
+router.route('/:id').get(protect, orderCtrl.getOrderById)
+router.route('/:id/pay').put(protect, orderCtrl.updateOrderToPaid)
 
 module.exports = router
