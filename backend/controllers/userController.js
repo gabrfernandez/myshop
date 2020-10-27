6de63fec5538e94a09736cf54086edaf8efd319d
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-10-26 23:07:26 -0700

    delete user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -103,4 +103,29 @@ const updateUserProfile = asyncHandler(async (req, res) => {
         throw new Error('User not found')
     }
 })
-module.exports = { authUser, getUserProfile, registerUser, updateUserProfile }
+
+//@desc get all users
+//@route Get /api/users
+//@access Private/admin
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users)
+
+})
+
+//@desc delete a user
+//@route delete /api/users/:id
+//@access Private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if (user) {
+        await user.remove()
+        res.json({ message: 'User removed' })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+})
+module.exports = { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const userCtrl = require('../controllers/userController')
-const { protect } = require('../middleware/authMiddleware')
+const { protect, admin } = require('../middleware/authMiddleware')
 
-router.route('/').post(userCtrl.registerUser)
+router.route('/').post(userCtrl.registerUser).get(protect, admin, userCtrl.getUsers)
 router.route('/login').post(userCtrl.authUser)
 router.route('/profile').get(protect, userCtrl.getUserProfile).put(protect, userCtrl.updateUserProfile)
-
+router.route('/:id').delete(protect, admin, userCtrl.deleteUser)
 
 module.exports = router
