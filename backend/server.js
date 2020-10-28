207ba199e14e58ba1f3e00d3e670f1245c41bfda
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-10-27 19:58:59 -0700

    products finished

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express') //common js
 const dotenv = require('dotenv')
 const colors = require('colors')
@@ -8,6 +9,7 @@ const connectDB = require('./config/db')
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
 const orderRoutes = require('./routes/orderRoutes')
+const uploadRoutes = require('./routes/uploadRoutes')
 
 
 dotenv.config()
@@ -25,8 +27,12 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+//const __dirname=path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
