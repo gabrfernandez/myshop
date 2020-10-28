93761383974007d1756daf147cba9b644e7b369a
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-10-27 22:50:53 -0700

    product review

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ const { protect, admin } = require('../middleware/authMiddleware')
 
 
 router.route('/').get(productCtrl.getProducts).post(protect, admin, productCtrl.createProduct)
+router.route('/:id/reviews').post(protect, productCtrl.createProductReview)
 router.route('/:id').get(productCtrl.getProductById).delete(protect, admin, productCtrl.deleteProduct).put(protect, admin, productCtrl.updateProduct)
 
 module.exports = router
