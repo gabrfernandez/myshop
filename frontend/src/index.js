ed5edb04ebcfe453f30924a78a768ce7d081d2a2
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-11-03 16:36:03 -0800

    styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux'
 import store from './store'
-import "bootswatch/dist/lux/bootstrap.min.css";
+import "bootswatch/dist/yeti/bootstrap.min.css";
 // import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
